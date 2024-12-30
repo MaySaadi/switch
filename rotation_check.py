90eb6f64540bb0c96e2828ca2f498ee8a6e0fe66
maydarawshi <may.darawshi19@gmail.com>
2024-12-30 10:54:07 +0200

    added new problem solving

--- rotation_check.py
+++ rotation_check.py
@@ -9,3 +9,14 @@ def is_rotation(word1, word2):
 print(is_rotation("",""))
 
 
+def is_rotation_shift(word1, word2):
+    if len(word1) != len(word2):
+        return False
+
+    rotated = word1
+    for _ in range(len(word1)):
+        rotated = rotated[1:] + rotated[0]  
+        if rotated == word2:
+            return True
+
+    return False
